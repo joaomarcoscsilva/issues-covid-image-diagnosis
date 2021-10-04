270b1ecb97cb8b223689c39fff23d6deeb7cc2b5
PedroMartelleto <martelleto.pedro@gmail.com>
2021-10-04 16:04:22 +0000

    Embeddings closest to threshold plot

--- plots.py
+++ plots.py
@@ -2,10 +2,31 @@ import seaborn as sns
 import sklearn.metrics
 import utils
 import matplotlib.pyplot as plt
+import math
 
 def confusion_matrix(dataset, y_test_pred, title):
     sns.heatmap(sklearn.metrics.confusion_matrix(dataset.y_test[0:y_test_pred.shape[0],].argmax(1), y_test_pred.argmax(1), normalize = 'true'),
                 annot = True,
                 xticklabels = utils.CLASS_NAMES,
                 yticklabels = utils.CLASS_NAMES).set(title = title)
+    plt.show()
+
+def compare_images(images_a, images_b, rows):
+    fig = plt.figure(figsize=(32, 24))
+
+    columns = 10
+
+    for i in range(int(rows * columns / 2)):
+        if i >= images_a.shape[0]:
+            plt.show()
+            return
+
+        fig.add_subplot(rows, columns, i*2+1)
+        plt.imshow(images_a[i,])
+        plt.axis('off')
+
+        fig.add_subplot(rows, columns, i*2+2)
+        plt.imshow(images_b[i,])
+        plt.axis('off')
+
     plt.show()
--- trim_duplicates.py
+++ trim_duplicates.py
@@ -41,40 +41,12 @@ def plot_similarities(dataset, sims, threshold=0.99):
     first_fig, axs = plt.subplots(1, 2, figsize = (15,5))
     
     max_sim = sims.max(0)
-    sns.histplot(max_sim, ax = axs[0]).set(title = 'Distribution of the maximum similarity for the images in the dataset')
+    sns.histplot(max_sim[max_sim > 0.95], ax = axs[0]).set(title = 'Distribution of the maximum similarity for the images in the dataset')
 
     # Plots 'duplicates' according to threshold
     plt.title('Images with similarity greater than ' + str(threshold) + ' in each class')
     pd.Series(dataset.y_all[:sims.shape[0],][max_sim > threshold].argmax(1)).map(dict(zip(range(len(utils.CLASS_NAMES)), utils.CLASS_NAMES))).hist()
     first_fig.show()
-    
-    columns = 5
-    rows = 5
-    num_imgs_to_show = columns*rows
-
-    # Gets distance of similarities to thresh
-    thresh_dist = max_sim - threshold
-    # Artificially increases dist of similarities smaller than threshold
-    thresh_dist = np.array((thresh_dist < 0) * 10000 + thresh_dist)
-
-    # Picks the N images with the smallest distance
-    smallest_idx = np.argpartition(thresh_dist, num_imgs_to_show)
-
-    # TODO: assert np.all(max_sim[smallest_idx,] > 0)
-    second_fig = plt.figure(figsize=(16, 16))
-    
-    for j in range(0, int((columns*rows)/2)):
-        img_idx = smallest_idx[j]
-        img = dataset.x_all[img_idx,]
-        second_fig.add_subplot(rows, columns, j*2+1)
-        plt.imshow(img)
-
-        idx_of_largest_sim = sims[img_idx,].argmax()
-        similar_img = dataset.x_all[idx_of_largest_sim,]
-        second_fig.add_subplot(rows, columns, j*2+2)
-        plt.imshow(similar_img)
-    
-    plt.show()
 
 def remove_duplicates(dataset, sims, threshold=0.99):
     remove = set()
